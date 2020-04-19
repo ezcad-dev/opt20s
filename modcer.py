f963d25e584807b5bd6fb4031e4d6b9d08b15055
Ezcad Team <ezcad.dev@gmail.com>
2020-04-19 14:58:41 -0500

    Add modeling Rps

--- modcer.py
+++ modcer.py
@@ -12,6 +12,71 @@ def main():
     pass
 
 
+def rps_cer1977(r1, r2, r3, r4, inc_angle, amp_type='real'):
+    """
+    Calculate Rps using Zoeppritz equation, explicit and exact, Zhu 2014.
+
+    Parameters
+    ----------
+    r1 : float
+        Vp2 / Vp1
+    r2 : float
+        Vs1 / Vp1
+    r3 : float
+        Vs2 / Vp1
+    r4 : float
+        Ro2 / Ro1
+    inc_angle : float
+        incident angle in degrees
+    amp_type : str
+        amplitude type, 'abs' for absolute value, 'real' for the real part
+        of a complex number which preserves the sign.
+
+    Returns
+    -------
+    amp : float
+        Amplitude
+    pha : float
+        Phase in degrees
+    """
+    if inc_angle < 0 or inc_angle >= 90:
+        raise ValueError("Wrong angle value")
+
+    if inc_angle == 0:
+        # normal incidence, no PS conversion
+        amp, pha = 0, 0
+        return amp, pha
+
+    angle = inc_angle / 180. * pi
+
+    r0 = 1  # dummy, Vp1 / Vp1
+    CT0 = r0 * sin(angle) / complex_sqrt(r0, angle)
+    CT1 = r1 * sin(angle) / complex_sqrt(r1, angle)
+    CT2 = r2 * sin(angle) / complex_sqrt(r2, angle)
+    CT3 = r3 * sin(angle) / complex_sqrt(r3, angle)
+
+    Q = 2 * sin(angle) ** 2 * (r4 * r3 ** 2 - r2 ** 2)
+    A = (r4 - Q) ** 2 * CT1 * CT3
+    B = (r4 - Q - 1) ** 2 * CT0 * CT1 * CT2 * CT3
+    C = (1 + Q) ** 2 * CT0 * CT2
+    D = r4 * CT1 * CT2
+    E = r4 * CT0 * CT3
+    F = Q ** 2
+    G = Q * (1 + Q)
+    H = (r4 - Q) * (r4 - Q - 1) * CT1 * CT3
+
+    upp = 2 * CT2 * (G + H) / r2
+    low = F + E + D + C + B + A
+    rpp = upp / low
+    pha = phase(rpp) * 180. / pi
+    if amp_type is 'real':
+        return rpp.real, pha
+    elif amp_type is 'abs':
+        return abs(rpp), pha
+    else:
+        raise ValueError("Unknown amplitude type")
+
+
 def rpp_cer1977(r1, r2, r3, r4, inc_angle, amp_type='real'):
     """
     Calculate Rpp using Zoeppritz equation, explicit and exact, Zhu 2012.
--- test_modcer.py
+++ test_modcer.py
@@ -4,7 +4,7 @@
 import unittest
 # import numpy as np
 from utils import elapar_hs2ratio
-from modcer import rpp_cer1977
+from modcer import rpp_cer1977, rps_cer1977
 
 
 class Test(unittest.TestCase):
@@ -34,7 +34,26 @@ class Test(unittest.TestCase):
         self.assertLessEqual(err, 0.001)
 
     def test_2(self):
-        self.assertTrue(True)
+        # Two half spaces elastic model
+        vp1, vp2 = 5.72, 2.87
+        vs1, vs2 = 2.93, 1.61
+        ro1, ro2 = 2.86, 2.14
+
+        # Change parameterization
+        r1, r2, r3, r4 = elapar_hs2ratio(vp1, vs1, ro1, vp2, vs2, ro2)
+
+        # Define angles
+        # angles = np.arange(0, 90, 1)
+        # for angle in angles:
+        #     amp, pha = rps_cer1977(r1, r2, r3, r4, angle, amp_type='abs')
+        #     amp, pha = rps_cer1977(r1, r2, r3, r4, angle, amp_type='real')
+        #     print("ang,amp,pha =", angle, amp, pha)
+
+        angle = 10
+        amp, pha = rps_cer1977(r1, r2, r3, r4, angle, amp_type='real')
+        amp_truth = -0.14823324
+        err = amp_truth - amp
+        self.assertLessEqual(err, 0.001)
 
 
 if __name__ == '__main__':
