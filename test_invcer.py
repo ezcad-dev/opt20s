38d0ee4271790339df35c6a9810c98b51ab7de14
Ezcad Team <ezcad.dev@gmail.com>
2020-04-19 23:45:19 -0500

    Add test showing joint PP/PS more robust to noise

--- test_invcer.py
+++ test_invcer.py
@@ -51,6 +51,56 @@ class Test(unittest.TestCase):
         err = r1 - x_new[0]
         self.assertLessEqual(err, 0.001)
 
+    def test_pp_noise(self):
+        # Two half spaces elastic model
+        vp1, vp2 = 4.0, 2.0
+        vs1, vs2 = 2.0, 1.0
+        ro1, ro2 = 2.4, 2.0
+
+        # Change parameterization
+        r1, r2, r3, r4 = elapar_hs2ratio(vp1, vs1, ro1, vp2, vs2, ro2)
+
+        # Define angles
+        angles = np.arange(1, 60, 1)
+
+        # Calculate the reflection amplitude or b in Ax=b
+        m = len(angles)
+        rpp = np.zeros(m)
+        rps = np.zeros(m)
+        for i in range(m):
+            angle = angles[i]
+            amp, pha = rpp_cer1977(r1, r2, r3, r4, angle)
+            rpp[i] = amp
+            amp, pha = rps_cer1977(r1, r2, r3, r4, angle, amp_type='abs')
+            rps[i] = amp
+
+        # Add noise to data
+        ramp = np.max(rpp) - np.min(rpp)
+        mu, sigma = 0, 0.05 * ramp  # mean and standard deviation
+        noise = np.random.normal(mu, sigma, m)
+        rpp_noisy = rpp + noise
+        ramp = np.max(rps) - np.min(rps)
+        mu, sigma = 0, 0.05 * ramp  # mean and standard deviation
+        noise = np.random.normal(mu, sigma, m)
+        rps_noisy = rps + noise
+
+        print("Target model:", r1, r2, r3, r4)
+        r1_ini = 2.4 / 4.0
+        r2_ini = 2.2 / 4.0
+        r3_ini = 1.3 / 4.0
+        r4_ini = 1.6 / 2.4
+        x_ini = (r1_ini, r2_ini, r3_ini, r4_ini)
+        print("Initial model:", x_ini)
+
+        for i in range(10):
+            x_new = cer1itr(angles, rpp_noisy, x_ini, rps=None)
+            # x_new = cer1itr(angles, rpp_noisy, x_ini, rps=rps_noisy)
+            print("Updated", i, x_new)
+            x_ini = x_new
+
+        # err = r1 - x_new[0]
+        # self.assertLessEqual(err, 0.001)
+
 
 if __name__ == '__main__':
     unittest.main()
