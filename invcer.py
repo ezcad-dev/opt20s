e06f2cc78c94961180924aff906fb92420f267db
Ezcad Team <ezcad.dev@gmail.com>
2020-04-19 23:20:59 -0500

    Add Rps to inversion

--- invcer.py
+++ invcer.py
@@ -5,7 +5,7 @@ Linearized inversion
 """
 
 import numpy as np
-from modcer import rpp_cer1977
+from modcer import rpp_cer1977, rps_cer1977
 from gracer import gradient
 
 
@@ -13,7 +13,8 @@ def main():
     pass
 
 
-def cer1itr(angles, rpp, x_ini, scale=1, constraints={}):
+def cer1itr(angles, rpp, x_ini, rps=None, fm='numeric', scale=1,
+    constraints={}):
     """
     One iteration of linearized inversion.
 
@@ -25,6 +26,10 @@ def cer1itr(angles, rpp, x_ini, scale=1, constraints={}):
         Rpp amplitude at the angles, also the b in Ax=b.
     x_ini : tuple
         Initial or starting model of this iteration.
+    rps : array
+        Rps amplitude at the angles, append b in Ax=b.
+    fm : str
+        the method to calculate Frechet derivatives, numeric or analytic
     scale : float
         Scale to the model update
     constraints : dict
@@ -47,22 +52,41 @@ def cer1itr(angles, rpp, x_ini, scale=1, constraints={}):
     r1_ini, r2_ini, r3_ini, r4_ini = x_ini_copy
 
     m = len(angles)
+    n = 4
     rpp_ini = np.zeros(m)
-    A = np.zeros((m, 4))
+    A1 = np.zeros((m, n))
     for i in range(m):
         angle = angles[i]
         amp, pha = rpp_cer1977(r1_ini, r2_ini, r3_ini, r4_ini, angle)
         rpp_ini[i] = amp
-
         # Calculate the Jacobian matrix A in Ax=b
-        fr1 = gradient(r1_ini, r2_ini, r3_ini, r4_ini, angle, 'PP', 1)
-        fr2 = gradient(r1_ini, r2_ini, r3_ini, r4_ini, angle, 'PP', 2)
-        fr3 = gradient(r1_ini, r2_ini, r3_ini, r4_ini, angle, 'PP', 3)
-        fr4 = gradient(r1_ini, r2_ini, r3_ini, r4_ini, angle, 'PP', 4)
-        A[i] = [fr1, fr2, fr3, fr4]
-
+        for j in range(n):
+            fre = gradient(r1_ini, r2_ini, r3_ini, r4_ini, angle,
+                'PP', j+1, method=fm)
+            A1[i, j] = fre
     # A *= -1  # needed when we take abs of negative rpp
-    b_dif = rpp - rpp_ini
+    b1 = rpp - rpp_ini
+
+    if rps is None:
+        A = A1
+        b_dif = b1
+    else:
+        rps_ini = np.zeros(m)
+        A2 = np.zeros((m, n))
+        for i in range(m):
+            angle = angles[i]
+            amp, pha = rps_cer1977(r1_ini, r2_ini, r3_ini, r4_ini, angle,
+                amp_type='abs')
+            rps_ini[i] = amp
+            # Calculate the Jacobian matrix A in Ax=b
+            for j in range(n):
+                fre = gradient(r1_ini, r2_ini, r3_ini, r4_ini, angle,
+                    'PS', j+1, method=fm)
+                A2[i, j] = fre
+        b2 = rps - rps_ini
+        A = np.concatenate((A1, A2), axis=0)
+        b_dif = np.concatenate((b1, b2), axis=0)
+
     lstsq = np.linalg.lstsq(A, b_dif, rcond=None)
     x_dif = lstsq[0]
     if 'r1' in constraints:
--- test_invcer.py
+++ test_invcer.py
@@ -4,7 +4,7 @@
 import unittest
 import numpy as np
 from utils import elapar_hs2ratio
-from modcer import rpp_cer1977
+from modcer import rpp_cer1977, rps_cer1977
 from invcer import cer1itr
 
 
@@ -27,10 +27,13 @@ class Test(unittest.TestCase):
         # Calculate the reflection amplitude or b in Ax=b
         m = len(angles)
         rpp = np.zeros(m)
+        rps = np.zeros(m)
         for i in range(m):
             angle = angles[i]
             amp, pha = rpp_cer1977(r1, r2, r3, r4, angle)
             rpp[i] = amp
+            amp, pha = rps_cer1977(r1, r2, r3, r4, angle, amp_type='abs')
+            rps[i] = amp
 
         print("Target model:", r1, r2, r3, r4)
         r1_ini = 2.4 / 4.0
@@ -41,7 +44,7 @@ class Test(unittest.TestCase):
         print("Initial model:", x_ini)
 
         for i in range(5):
-            x_new = cer1itr(angles, rpp, x_ini)
+            x_new = cer1itr(angles, rpp, x_ini, rps=rps)
             print("Updated", x_new)
             x_ini = x_new
 
